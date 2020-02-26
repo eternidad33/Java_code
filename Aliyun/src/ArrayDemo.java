ececbc34b22d72aacc5918d787c40f89c22623f7
enternity <2898693753@qq.com>
2020-02-26 20:31:51 +0800

    最大值最小值

--- Aliyun/src/ArrayDemo.java
+++ Aliyun/src/ArrayDemo.java
@@ -8,11 +8,29 @@ public class ArrayDemo {
     public static void main(String[] args) {
         int data[]=initArray();
         printArray(data);
+        int sum = 0;
+        int min=data[0];
+        int max=data[0];
+        double avg = 0;
+        for(int num:data){
+            sum+=num;
+            if(num>max)
+                max=num;
+            if(num<min)
+                min=num;
+        }
+        avg=sum/data.length;
+        System.out.println("数组的和为："+sum);
+        System.out.println("数组的平均值为："+avg);
+        System.out.println("数组的最大值为："+max);
+        System.out.println("数组的最小值为："+min);
+
     }
     public static int[] initArray(){
         int arr[]=new int[]{1,2,3,4,5};
         return arr;
     }
+
     public static void printArray(int[] temp){
         for(int t:temp)
             System.out.println(t);
