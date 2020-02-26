469ed7df4a53111a2d7259b8630fbdd9c336126d
enternity <2898693753@qq.com>
2020-02-26 18:01:36 +0800

    int数组默认为0

---
+++ Aliyun/src/ArrayDemo.java
@@ -0,0 +1,15 @@
+/**
+ * @author Vigilr
+ * @project Java_code
+ * @package PACKAGE_NAME
+ * @time 2020-02-26 5:31 pm
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int a[]=new int[3];
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+
+    }
+}
