1f6e77b05918dcdbe5a1ae1c452cc602277f532e
enternity <2898693753@qq.com>
2020-02-28 10:44:04 +0800

    其他字符串相关的方法

--- Aliyun/src/StringDemo.java
+++ Aliyun/src/StringDemo.java
@@ -4,15 +4,38 @@
  * @package PACKAGE_NAME
  * @time 2020-02-27 9:29 pm
  */
+class StringUtil{
+    public static String initCap(String str){//将字符串首字母大写，其余字母小写
+        if(str==null||"".equals(str))
+            return str;
+        return str.substring(0,1).toUpperCase()+str.substring(1).toLowerCase();
+    }
+}
+
+
+
 public class StringDemo {
 
     public static void main(String[] args) {
+        String str1 = "hello ";
+        String str2 = "Java";
+        String str3="";
+        System.out.println(str1.concat(str2));//字符串的连接
+        System.out.println(str1.isEmpty());//判断字符串内容是否为空
+        System.out.println(str3.isEmpty());
+        String str4="         Hello Java     ";
+        String trimStr4=str4.trim();//去掉字符串中所有空格
+        System.out.println(str4.length());
+        System.out.println(trimStr4.length());
+        System.out.println(str4);
+        System.out.println(trimStr4);
+        System.out.println(str1.toUpperCase());//全部转换成大写
+        System.out.println(str2.toLowerCase());//全部转换成小写
+        String a="aSDDasdF";
+        String b="m";
+        System.out.println(StringUtil.initCap(a));
+        System.out.println(StringUtil.initCap(b));
 
-        String name="小明";
-        int age=10;
-        double score=59.123456789;
-        String str=String.format("姓名：%s，年龄：%d，分数：%5.2f",name,age,score);//format是一个静态方法
-        System.out.println(str);
 
     }
 
