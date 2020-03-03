ff71d7aee936b9d14a4f4500ebc104d8a2f89dd9
vigilr <3496655347@qq.com>
2020-03-04 00:06:10 +0800

    工厂模式使用泛型方法

--- README.md
+++ README.md
@@ -405,7 +405,32 @@ public static <T>T[] fun(T ... args){
     return args;
 }
 ```
-
+工厂模式中使用泛型方法
+```java
+interface IMessage{
+    public void send(String str);
+}
+class MessageImpl implements IMessage{
+    @Override
+    public void send(String str) {
+        System.out.println("消息发送"+str);
+    }
+}
+class Factory{
+    public static  <T>T getInstance(String className){
+        if ("messageimpl".equalsIgnoreCase(className)){
+            return (T) new MessageImpl();
+        }
+        return null;
+    }
+}
+public class JavaDemo {
+    public static void main(String[] args) {
+        IMessage msg=Factory.getInstance("messageimpl");
+        msg.send("java");
+    }
+}
+```
 
 ---
 ## Java高级进阶
--- src/object/JavaDemo.java
+++ src/object/JavaDemo.java
@@ -7,15 +7,26 @@ package object;
  * @package object
  * @time 2020-03-01 12:35 pm
  */
-
+interface IMessage{
+    public void send(String str);
+}
+class MessageImpl implements IMessage{
+    @Override
+    public void send(String str) {
+        System.out.println("消息发送"+str);
+    }
+}
+class Factory{
+    public static  <T>T getInstance(String className){
+        if ("messageimpl".equalsIgnoreCase(className)){
+            return (T) new MessageImpl();
+        }
+        return null;
+    }
+}
 public class JavaDemo {
     public static void main(String[] args) {
-        Integer nums[]=fun(1,2,3);
-        for (int num:nums){
-            System.out.println(num);
-        }
-    }
-    public static <T>T[] fun(T ... args){
-        return args;
+        IMessage msg=Factory.getInstance("messageimpl");
+        msg.send("java");
     }
 }
