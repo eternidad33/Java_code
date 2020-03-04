2d0fc3d3a63c2c9df1c0e9ad2cb4aa8e9bb02467
vigilr <3496655347@qq.com>
2020-03-04 18:43:07 +0800

    遍历枚举

--- src/object/JavaDemo.java
+++ src/object/JavaDemo.java
@@ -7,9 +7,12 @@ package object;
  * @package object
  * @time 2020-03-01 12:35 pm
  */
+enum Color {RED, BLUE, GREEN}
 
 public class JavaDemo {
     public static void main(String[] args) {
-
+        for (Color c : Color.values()) {
+            System.out.println(c);
+        }
     }
 }
