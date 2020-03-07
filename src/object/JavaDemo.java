f4123778a30f2449fa75bf6603ebe2e977e9c9d6
vigilr <3496655347@qq.com>
2020-03-07 20:04:36 +0800

    update

--- src/object/JavaDemo.java
+++ src/object/JavaDemo.java
@@ -11,13 +11,6 @@ import java.util.function.*;
 
 public class JavaDemo {
     public static void main(String[] args) throws Exception {
-//        Function<String,Boolean> fun="hellojava"::startsWith;
-//        System.out.println(fun.apply("hello"));
-//        Consumer<String> fun=System.out::println;
-//        fun.accept("HelloJava");
-//        Supplier<String> fun="hellojava"::toUpperCase;
-//        System.out.println(fun.get());
-        Predicate<String> fun="hello"::equals;
-        System.out.println(fun.test("hello"));
+
     }
 }
