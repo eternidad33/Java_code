102eec18a52023dba8ccbab69e41ea580d121aa5
vigilr <3496655347@qq.com>
2020-03-11 19:24:23 +0800

    AWTpanel

--- src/mygui/AWTDemo.java
+++ src/mygui/AWTDemo.java
@@ -12,8 +12,14 @@ public class AWTDemo extends Frame {
     public static void main(String[] args) {
         AWTDemo awt=new AWTDemo("第一个AWT窗口");
         awt.setSize(500,500);
+        awt.setLayout(null);//取消布局管理器
         awt.setLocation(700,200);
         awt.setBackground(Color.cyan);
+        Panel pan=new Panel();
+        pan.setSize(100,100);
+        pan.setBackground(Color.GREEN);
+        pan.setLocation(100,100);
+        awt.add(pan);
         awt.setVisible(true);
     }
     public AWTDemo(String str){
