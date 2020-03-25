abe1e5861fbee4a720a518e6cea15f545882e5db
vigilr <3496655347@qq.com>
2020-03-25 19:50:31 +0800

    线程操作方法

---
+++ src/thread/ThreadTest.java
@@ -0,0 +1,14 @@
+package thread;
+
+/**
+ * 项目名 Java_code
+ * <br>包名 thread
+ * <br>作者 Vigilr
+ * <br>创建时间 2020/3/25 18:24
+ * <br>描述
+ */
+public class ThreadTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getPriority());
+    }
+}
