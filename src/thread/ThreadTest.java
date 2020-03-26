e2b9e71807a6405ac0d2c9e91fa9efa1d1e91272
vigilr <3496655347@qq.com>
2020-03-26 14:54:24 +0800

    更新README

--- src/thread/ThreadTest.java
+++ src/thread/ThreadTest.java
@@ -7,8 +7,35 @@ package thread;
  * <br>创建时间 2020/3/25 18:24
  * <br>描述
  */
+class th implements Runnable {
+    public static int ticket = 10;
+    public synchronized boolean sale(){
+        if (ticket > 0) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "当前票数为：" + ticket--);
+            return true;
+        } else {
+            System.out.println("票卖光了");
+            return false;
+        }
+    }
+    @Override
+    public void run() {
+        while (this.sale()){
+            ;
+        }
+    }
+}
+
 public class ThreadTest {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.currentThread().getPriority());
+        th t = new th();
+        new Thread(t, "线程1").start();
+        new Thread(t, "线程2").start();
+        new Thread(t, "线程3").start();
     }
 }
