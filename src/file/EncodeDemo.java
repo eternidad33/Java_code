28b6834591c9ad352a985f73f108132fdd3da5d2
vigilr <3496655347@qq.com>
2020-04-01 16:14:48 +0800

    字符编码

---
+++ src/file/EncodeDemo.java
@@ -0,0 +1,15 @@
+package file;
+
+/**
+ * 项目名 Java_code
+ * <br>包名 file
+ * <br>创建时间 2020/4/1 15:38
+ * <br>描述
+ *
+ * @author Vigilr
+ */
+public class EncodeDemo {
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+    }
+}
